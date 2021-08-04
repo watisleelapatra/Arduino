ac13d908463bf26c78f7749524cf0cf9703d70ec
watisleelapatra <69517963+watisleelapatra@users.noreply.github.com>
2021-08-04 22:22:47 +0700

    Add files via upload

--- RGB.ino
+++ RGB.ino
@@ -1,36 +1,36 @@
-int R = 9;
-int G = 10;
-int B = 11;
+byte R = 9;
+byte G = 10;
+byte B = 11;
+
 void setup() {
-pinMode(R, OUTPUT);
-pinMode(G, OUTPUT);
-pinMode(B, OUTPUT);
-Serial.begin(9600);
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
 }
 void loop() {
-// กำหนด LED ดับ
-analogWrite(R, 0);
-analogWrite(G, 0);
-analogWrite(B, 0);
-delay(500);
-// กำหนด LED สีแดง
-analogWrite(R, 255);
-analogWrite(G, 0);
-analogWrite(B, 0);
-delay(500);
-// กำหนด LED สีเขียว
-analogWrite(R, 0);
-analogWrite(G, 0255);
-analogWrite(B, 0);
-delay(500);
-// กำหนด LED สีน้ำเงิน
-analogWrite(R, 0);
-analogWrite(G, 0);
-analogWrite(B, 255);
-delay(500);
-// กำหนด LED สีขาว
-analogWrite(R, 255);
-analogWrite(G, 255);
-analogWrite(B, 255);
-delay(500);
+  // All off
+  analogWrite(R, 0);
+  analogWrite(G, 0);
+  analogWrite(B, 0);
+  delay(500);
+  // Red ON
+  analogWrite(R, 255);
+  analogWrite(G, 0);
+  analogWrite(B, 0);
+  delay(500);
+  // Green ON
+  analogWrite(R, 0);
+  analogWrite(G, 0255);
+  analogWrite(B, 0);
+  delay(500);
+  // Blue ON
+  analogWrite(R, 0);
+  analogWrite(G, 0);
+  analogWrite(B, 255);
+  delay(500);
+  // All ON
+  analogWrite(R, 255);
+  analogWrite(G, 255);
+  analogWrite(B, 255);
+  delay(500);
 }
