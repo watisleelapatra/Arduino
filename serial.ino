ffe65f0d712ab24525cd0ff268cb2fb9d30e9c5b
watisleelapatra <69517963+watisleelapatra@users.noreply.github.com>
2021-08-07 14:12:20 +0700

    Add files via upload

---
+++ serial.ino
@@ -0,0 +1,10 @@
+ void setup() {
+    Serial.begin(9600);
+    Serial.println();
+    Serial.print("Hello!, ");
+    Serial.println("world");
+    Serial.print("3x7 = ");
+    Serial.println(3*7);
+}
+void loop() {
+}
